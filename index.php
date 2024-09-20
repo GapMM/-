73cb9e03e218211f083dbddb505fc5dbc44025e8
cany789 <kristinavelma16@gmail.com>
2024-09-20 15:57:24 +0300

    Create index.php
    20.09.2024

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$int = 1;
+$float = 1.13;
+$str = '1';
+
+$arr =[
+    'NAME' => 'bob',
+    'SECOND_NAME'=> 'Petrov'
+];
+
+
+echo // выводит только строчные элементы//
+var_dump(); 
+
+ 
